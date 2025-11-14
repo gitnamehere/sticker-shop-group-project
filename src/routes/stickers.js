1f8c63f98cc765ab749eb13a74ce5471592f4e50
gitnamehere <105529595+gitnamehere@users.noreply.github.com>
2025-11-13 19:47:29 -0500

    add get a sticker by an id route, fix get all stickers route

--- src/routes/stickers.js
+++ src/routes/stickers.js
@@ -5,8 +5,16 @@ const stickersRouter = express.Router();
 
 stickersRouter.get("/all", (req, res) => {
   pool.query("SELECT * FROM sticker").then((result) => {
-    res.send(result);
+    res.send(result.rows);
   })
-})
+});
+
+stickersRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  pool.query("SELECT * FROM sticker WHERE sticker_id = $1", [id]).then((result) => {
+    if (result.rows) res.send(result.rows[0]);
+  });
+});
 
 export { stickersRouter };

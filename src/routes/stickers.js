457d7be1f9c11d652d5808ada408c2d8158096b3
Monique Huynh <47400590+mhxynh@users.noreply.github.com>
2025-11-18 19:57:45 -0500

    Created sticker DB routes

--- src/routes/stickers.js
+++ src/routes/stickers.js
@@ -15,4 +15,34 @@ stickersRouter.get("/:id", async (req, res) => {
   res.send(result).rows[0];
 });
 
+stickersRouter.get("/creator/:creator_id", async (req, res) => {
+  const { creator_id } = req.params;
+
+  const result = await pool.query("SELECT * FROM sticker WHERE creator_id = $1", [creator_id]);
+  res.send(result.rows);
+});
+
+stickersRouter.get("/image_sticker", async (req, res) => {
+  const result = await pool.query("SELECT * FROM sticker, image_sticker WHERE sticker.sticker_id = image_sticker.sticker_id");
+  res.send(result.rows);
+});
+
+stickersRouter.get("/polygonal", async (req, res) => {
+  const result = await pool.query("SELECT * FROM sticker, polygonal_sticker WHERE sticker.sticker_id = polygonal_sticker.sticker_id");
+  res.send(result.rows);
+});
+
+stickersRouter.post("/create", async (req, res) => {
+  const { creator_id, name, description, image_data } = req.body;
+
+  if (!creator_id || !name || !description || !image_data) {
+    return res.status(400).send("All fields are required");
+  }
+
+  const date_created = CurrentDate();
+
+  const result = await pool.query( "INSERT INTO sticker (creator_id, name, description, date_created) VALUES ($1, $2, $3, $4)", [creator_id, name, description, date_created] );
+  await pool.query("INSERT INTO image_sticker (sticker_id, image_data) VALUES ($1, $2);", [result.rows[0].sticker_id, image_data]);
+});
+
 export { stickersRouter };
